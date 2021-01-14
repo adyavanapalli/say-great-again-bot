00d532559d037ea14f03732fba4afacc115c94aa
Anand Dyavanapalli <adyavanapalli@gmail.com>
2021-01-13 22:41:42 -0500

    refactor: Simplify environment variable for bot token.

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import os
 from saygreatagainbot import Bot
 
 if __name__ == "__main__":
-    bot = Bot(os.environ.get("SAY_GREAT_AGAIN_BOT_TOKEN"))
+    bot = Bot(os.environ.get("TOKEN"))
     bot.start()
